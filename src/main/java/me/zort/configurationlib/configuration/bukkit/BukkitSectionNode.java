886f84fecec6b4274e464e7676cfc785e5a78ecf
ZorT <67344817+ZorTik@users.noreply.github.com>
2022-09-01 18:45:45 +0200

    Items.

--- src/main/java/me/zort/configurationlib/configuration/bukkit/BukkitSectionNode.java
+++ src/main/java/me/zort/configurationlib/configuration/bukkit/BukkitSectionNode.java
@@ -5,6 +5,8 @@ import me.zort.configurationlib.configuration.Node;
 import me.zort.configurationlib.configuration.SectionNode;
 import me.zort.configurationlib.util.Colorizer;
 import me.zort.configurationlib.util.ItemValidator;
+import org.apache.commons.lang.ArrayUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.configuration.ConfigurationSection;
@@ -12,10 +14,14 @@ import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 import org.jetbrains.annotations.Nullable;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class BukkitSectionNode extends SectionNode<ConfigurationSection> {
@@ -62,21 +68,30 @@ public class BukkitSectionNode extends SectionNode<ConfigurationSection> {
         if(!ItemValidator.validate(section)) {
             return null;
         }
-        Material type = Material.matchMaterial(section.getString("type"));
-        if(type == null) {
-            return null;
-        }
-        short data = (short) section.getInt("data");
+        ItemStack item;
+        ItemMeta meta;
         int amount = section.getInt("amount", 1);
-        ItemStack item = new ItemStack(type, amount, data);
-        ItemMeta meta = item.getItemMeta();
+        String typeString = section.getString("type");
+        if(typeString.startsWith("head-")) {
+            typeString = typeString.replaceFirst("head-", "");
+            // Bigger than 1.12
+            item = verMajor() > 112
+                    ? new ItemStack(Material.matchMaterial("PLAYER_HEAD"), amount)
+                    : new ItemStack(Material.matchMaterial("SKULL_ITEM"), amount);
+            meta = item.getItemMeta();
+            assignTextures(meta, typeString);
+        } else {
+            Material type = Material.matchMaterial(typeString);
+            if(type == null) {
+                return null;
+            }
+            short data = (short) section.getInt("data");
+            item = new ItemStack(type, amount, data);
+            meta = item.getItemMeta();
+        }
         if(meta != null) {
-            if(section.contains("name")) {
-                meta.setDisplayName(Colorizer.colorize(section.getString("display-name")));
-            }
-            if(section.contains("lore")) {
-                meta.setLore(Colorizer.colorize(section.getStringList("lore")));
-            }
+            if(section.contains("name")) meta.setDisplayName(Colorizer.colorize(section.getString("display-name")));
+            if(section.contains("lore")) meta.setLore(Colorizer.colorize(section.getStringList("lore")));
             if(section.contains("enchanted") && section.getBoolean("enchanted")) {
                 meta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 1, true);
                 meta.addItemFlags(ItemFlag.values());
@@ -105,6 +120,27 @@ public class BukkitSectionNode extends SectionNode<ConfigurationSection> {
         return item;
     }
 
+    private void assignTextures(ItemMeta unknownMeta, String value) {
+        SkullMeta meta = (SkullMeta) unknownMeta;
+        try {
+            Object profile = Class.forName("com.mojang.authlib.GameProfile")
+                    .getDeclaredConstructor(UUID.class, String.class)
+                    .newInstance(UUID.randomUUID(), "");
+
+            Object propertyMap = profile.getClass().getDeclaredMethod("getProperties").invoke(profile);
+            Object property = Class.forName("com.mojang.authlib.properties.Property")
+                    .getDeclaredConstructor(String.class, String.class).newInstance("textures", value);
+            propertyMap.getClass().getMethod("put", Object.class, Object.class).invoke(propertyMap, "textures", property);
+
+            Field metaProfileField = meta.getClass().getDeclaredField("profile");
+            metaProfileField.setAccessible(true);
+            metaProfileField.set(meta, profile);
+        } catch (ClassNotFoundException | NoSuchMethodException | InstantiationException | IllegalAccessException |
+                 InvocationTargetException | NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
     public String getPath() {
         return section.getCurrentPath();
@@ -122,6 +158,11 @@ public class BukkitSectionNode extends SectionNode<ConfigurationSection> {
                     ? new BukkitSectionNode(section.getConfigurationSection(key))
                     : new BukkitSimpleNode(section, key, section.get(key)));
         }
+    }
+
+    private static int verMajor() {
+        String verString = String.join("", (String[]) ArrayUtils.subarray(Bukkit.getServer().getBukkitVersion().split("-")[0].split("\\."), 0, 2));
+        return Integer.parseInt(verString);
     }
 
 }
