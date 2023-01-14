b0fd673b6081036877badc350ce7e0cff78aba8f
ZorT <67344817+ZorTik@users.noreply.github.com>
2023-01-15 00:03:37 +0100

    Improvement.

--- src/main/java/me/zort/configurationlib/ConfigurationLib.java
+++ src/main/java/me/zort/configurationlib/ConfigurationLib.java
@@ -1,6 +1,7 @@
 package me.zort.configurationlib;
 
 import me.zort.configurationlib.configuration.bukkit.BukkitFileConfigurationNode;
+import org.bukkit.plugin.Plugin;
 
 import java.io.File;
 
@@ -11,6 +12,10 @@ public final class ConfigurationLib {
 
     public static BukkitFileConfigurationNode bukkit(File file) {
         return ConfigurationLibBukkit.of(ConfigurationLibBukkit.prepareDataFile(file));
+    }
+
+    public static BukkitFileConfigurationNode bukkit(Plugin plugin, String name) {
+        return ConfigurationLibBukkit.of(ConfigurationLibBukkit.prepareDataFile(plugin, name));
     }
 
 }
